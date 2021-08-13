6546d4ae887f04a0477555c318d6970a77d9dde6
Jai Kumar Dadhich <84243610+coderRj42@users.noreply.github.com>
2021-08-14 00:01:26 +0530

    Create count set bit .java

---
+++ bit/count set bit .java
@@ -0,0 +1,22 @@
+ 
+import java.io.*; 
+
+class countSetBits { 
+ 
+	static int countSetBits(int n) 
+	{ 
+		int count = 0; 
+    	while (n != 0) {
+	      if (n % 2 != 0)
+	        count++;
+	      n = n/2;      
+	    } 
+	    return count; 
+	} 
+ 
+	public static void main(String args[]) 
+	{ 
+		int i = 9; 
+		System.out.println(countSetBits(i)); 
+	} 
+}
